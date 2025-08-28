67f155cd8ccda92a8ed2a553450ac566dd7cf12d
Dmytro Pogrebniak <33512974+dimaP8889@users.noreply.github.com>
2025-08-28 10:34:56 +0800

    feat: integrate request correlation

--- beautyn-api/src/app.module.ts
+++ beautyn-api/src/app.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import {
+  LoggerInterceptor,
+  LoggerModule,
+  RequestCorrelationMiddleware,
+} from '@shared/logger';
 import { ApiGatewayModule } from './api-gateway/api-gateway.module';
 import { SharedModule } from './shared/shared.module';
 import { SupabaseModule } from './shared/supabase/supabase.module';
 
 @Module({
-  imports: [ApiGatewayModule, SharedModule, SupabaseModule],
+  imports: [ApiGatewayModule, SharedModule, SupabaseModule, LoggerModule],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggerInterceptor }],
 })
-export class AppModule {}
-
-
-
-
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestCorrelationMiddleware).forRoutes('*');
+  }
+}
